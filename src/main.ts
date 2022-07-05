785c4f9a32a79ed21772be86ce60febf8dd2ee26
afdavletkulov <afdavletkulov@rttv.ru>
2022-07-05 22:13:47 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
-  //TODO: временно 
+  //TODO: временно !!!
   app.enableCors();
   await app.listen(9000);
 }
