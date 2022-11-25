428ea75aff438574797b98155924482e20bb15ff
gaisdav <gais.davletkildin@gmail.com>
2022-11-25 18:54:23 +0500

    added admin module

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ValidateException } from './customExeptions';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { isDev } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
-    credentials: true,
-  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       exceptionFactory: (fields) => {
@@ -19,16 +17,23 @@ async function bootstrap() {
   );
   app.setGlobalPrefix('api');
 
-  const options = new DocumentBuilder()
-    .setTitle('Yul-yort API')
-    .setDescription('The yul-yort API description')
-    .setVersion('1.0')
-    .addTag('yul-yort')
-    .addBearerAuth()
-    .build();
+  if (isDev) {
+    app.enableCors({
+      origin: true,
+      credentials: true,
+    });
 
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('swagger', app, document);
+    const options = new DocumentBuilder()
+      .setTitle('Yul-yort API')
+      .setDescription('The yul-yort API description')
+      .setVersion('1.0')
+      .addTag('yul-yort')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('swagger', app, document);
+  }
 
   await app.listen(9000);
   console.log(`Application is running on: ${await app.getUrl()}`);
